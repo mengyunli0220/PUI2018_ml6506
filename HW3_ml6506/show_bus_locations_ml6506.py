10504cdeb62d63e838d897f17a72c7ed3f61612a
Mengyun Li <43006890+mengyunli0220@users.noreply.github.com>
2018-09-25 19:14:39 -0400

    Update show_bus_locations_ml6506.py

--- HW3_ml6506/show_bus_locations_ml6506.py
+++ HW3_ml6506/show_bus_locations_ml6506.py
@@ -10,5 +10,19 @@ except ImportError:
 key=sys.argv[1]
 bus_line=sys.argv[2]
 
+print(key)
+print(bus_line)
+
 bus_url="http://bustime.mta.info/api/siri/vehicle-monitoring.json?key="+key+"&VehicleMonitoringDetailLevel=calls&LineRef="+bus_line
-print
+
+print("Bus Line:"+bus_line)
+
+bus_num = len(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
+bus_num_str = "Number of Active Buses : "
+bus_x = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+
+print(bus_num_s, bus_num)
+  for i in range(len(bus_x)):
+    lat = bus_x[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    long = bus_x[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+     print("Bus "+str(i)+" is at latitude "+str(lat)+" and longitude "+str(long))
