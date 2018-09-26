071cda3b9bcdcaacc75a5d55b9a78e036088e5f5
Mengyun Li <43006890+mengyunli0220@users.noreply.github.com>
2018-09-25 23:11:11 -0400

    Update get_bus_info_ml6506.py

--- HW3_ml6506/get_bus_info_ml6506.py
+++ HW3_ml6506/get_bus_info_ml6506.py
@@ -19,6 +19,25 @@ bus_url="http://bustime.mta.info/api/siri/vehicle-monitoring.json?key="+key+"&Ve
 
 print("Bus Line:"+bus_line)
 
-response = urllib.urlopen(url)
-data = response.read().decode("utf-8")
-data = json.loads(data)
+js_file=requests.get(bus_url).json()
+buses=js_file['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+num_buses=len(buses)
+
+#create dataframe
+columns = ['Latitude','Longitude','Stop Name','Stop Status']
+dataframe = pd.DataFrame(columns=columns)
+for x in range(num_buses):
+    long=buses[x]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    lat=buses[x]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    try:
+        stop_nm=buses[x]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+        stop_sta=buses[x]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+#    if len(buses[x]['MonitoredVehicleJourney']['OnwardCalls'])==0:
+    except:
+        stop_nm="N/A"
+        stop_sta="N/A"
+#    else:
+    finally:
+        for_ap = pd.DataFrame([[lat,long,stop_nm,stop_sta]], columns=['Latitude','Longitude','Stop Name','Stop Status'])
+        dataframe=dataframe.append([for_ap])
+dataframe.to_csv(filename,header=True, index=False, encoding='utf-8')
